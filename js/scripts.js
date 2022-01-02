d570c8e61dbfafb4896195a3ebe200b91ec5e3fb
Megan Carisa <meganchoy@gmail.com>
2022-01-01 19:00:36 -0500

    Head, Foot, Home, Review, Comment complete

--- js/scripts.js
+++ js/scripts.js
@@ -23,3 +23,33 @@ $(function() {
         $("#loginModal").modal("show");
     })
 })
+
+
+/* Dropdown shows on hover for md and up, code from https://webdesign.tutsplus.com/tutorials/how-to-make-the-bootstrap-navbar-dropdown-work-on-hover--cms-33840 */
+    
+const $dropdown = $(".dropdown");
+const $dropdownToggle = $(".dropdown-toggle");
+const $dropdownMenu = $(".dropdown-menu");
+const showClass = "show";
+
+$(window).on("load resize", function() {
+if (this.matchMedia("(min-width: 768px)").matches) {
+    $dropdown.hover(
+    function() {
+        const $this = $(this);
+        $this.addClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "true");
+        $this.find($dropdownMenu).addClass(showClass);
+    },
+    function() {
+        const $this = $(this);
+        $this.removeClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "false");
+        $this.find($dropdownMenu).removeClass(showClass);
+    }
+    );
+} else {
+    $dropdown.off("mouseenter mouseleave");
+}
+});
+   
